eba439f8f79a9ec3bcebc24707b138d0cbe84cc7
Philippe Hässig <phil@nine.ch>
2017-12-19 13:55:36 +0100

    Fix puma config

--- config/puma.rb
+++ config/puma.rb
@@ -7,8 +7,6 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
-pidfile false
-
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 port        ENV.fetch("PORT") { 3000 }
@@ -16,6 +14,8 @@ port        ENV.fetch("PORT") { 3000 }
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
+
+directory '/home/beer/www/current' if ENV.fetch("RAILS_ENV") == "production"
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
